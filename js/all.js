4d0a8fca7e8ff7e565a932984b5c81b6aeeb516f
dream-gem <103253437+dream-gem@users.noreply.github.com>
2022-07-06 22:56:00 +0800

    v2版
    新增scss寫法，增加grid寫法

--- js/all.js
+++ js/all.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.question').click(function(event) {
+    $('.question').click(function (event) {
+        // 讓點擊到的 question 亮起來，其他question移除active樣式
+        $(this).toggleClass('active');
+        
+        // 讓點擊到的 question找到父元素li，再找裡面的 answer 判斷收闔
+        $(this).parent().find('.answer').slideToggle();
+        
+        // 自己以外的 answer 隱藏起來
+        $(this).parent().siblings().find('.answer').slideUp();
+        // 自己以外的 question 移除active樣式
+        $(this).parent().siblings().find('.question').removeClass('active');
+    
 
-    // 讓點擊到的 question 亮起來，其他question移除active樣式
-    $(this).toggleClass('active');
-
-    // 讓點擊到的 question找到父元素li，再找裡面的 answer 判斷收闔
-    $(this).parent().find('.answer').slideToggle();
-
-    // 自己以外的 answer 隱藏起來
-    $(this).parent().siblings().find('.answer').slideUp();
-    // 自己以外的 answer 移除u樣式
-    $(this).parent().siblings().find('.question').removeClass('active');
-
-  });
+    });
 });
